570a92eec473caecc614783feb647972b0d8cc80
sarahdasilva <sarahtlse@hotmail.fr>
2021-02-23 17:51:23 +0100

    facebook Login

--- app/src/main/java/com/example/go4lunch/FacebookLoginActivity.java
+++ app/src/main/java/com/example/go4lunch/FacebookLoginActivity.java
@@ -1,21 +1,123 @@
 package com.example.go4lunch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.example.go4lunch.ui.login.AccountActivity;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-
-
+import bolts.Task;
 
 
 public class FacebookLoginActivity extends AppCompatActivity {
+
+    private CallbackManager mCallbackManager;
+
+    private FirebaseAuth mAuth;
+
+    private static final String TAG = "FACELOG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebook_login);
 
+        //Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null){
+            updateUI();
+        }
 
     }
+
+    private void updateUI() {
+        Toast.makeText( FacebookLoginActivity.this,"You're logged in", Toast.LENGTH_LONG);
+        Intent accountIntent = new Intent(FacebookLoginActivity.this, AccountActivity.class);
+        startActivity(accountIntent);
+        finish();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCredential:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI();
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCredential:failure", task.getException());
+                        Toast.makeText(FacebookLoginActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    updateUI();
+
+                    // ...
+                });
+    }
+
+
 }
--- app/src/main/java/com/example/go4lunch/MainActivity.java
+++ app/src/main/java/com/example/go4lunch/MainActivity.java
@@ -2,17 +2,53 @@ package com.example.go4lunch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.go4lunch.ui.login.AccountActivity;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FirebaseAuth.getInstance().signOut();
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+
+        //if the user is not authenticated, send him to SignInActivity to authenticate first
+        //else send him to the dashboard
+        if (currentUser != null){
+            Intent menuIntent = new Intent(MainActivity.this, MenuFragment.class);
+            startActivity(menuIntent);
+            finish();
+
+        }else {
+            Intent signIntent = new Intent(MainActivity.this, FacebookLoginActivity.class);
+            startActivity(signIntent);
+            finish();
+        }
     }
+
 }
+
